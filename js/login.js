dbcb2a5bae3d3a636944d81915e050754660740b
bsw8220 <a49208220@gmail.com>
2021-12-29 18:03:17 +0900

    로컬 실행 시 CORS 에러 우회 시도

--- js/login.js
+++ js/login.js
@@ -1,3 +1,9 @@
+(function addEventToBtns(){
+    const loginBtn = document.querySelector('#submit_btn');
+    loginBtn.addEventListener('click', login)
+    // loginBtn.addEventListener('click', loginXhr)
+})();
+
 function checkId(form) {
     if (form.id.value == "") {
         alert("아이디를 입력해주세요.");
@@ -16,19 +22,52 @@ function checkPassword(form) {
     return true;
 }
 
-function login(){
-    const form = document.login_form;
-    // const promise = new Promise((resolve, reject) =>{
-    
-    // })
-    // promise
-    //     .then((value) => {
-
-    //     })
-    //     .catch(error => {
-    //         alert(error)
-    //     })
-    // if(checkId(form) && checkPassword(form)){
-    //     alert("로그인 성공");
-    // }
+function getUserData(){
+    const url = 'data/user.json'
+    return fetch(url)
+        .then(res => res.json())
+        .then(res => res.users)
+        .catch(error => console.log(error))
 }
+
+async function login(){
+    const form = document.login_form;
+    const userData = await getUserData();
+
+    if (!(checkId(form) && checkPassword(form))){
+        return;
+    }
+
+    let loginMatch = '';
+    userData.forEach(item => {
+        if(item.userId === form.id.value && item.password === form.pwd.value){
+            sessionStorage.setItem('currentLogin', JSON.stringify(item));
+            loginMatch += 'matched';
+            return;
+        } 
+    })
+
+    if(loginMatch === "matched"){
+        location.href = 'index.html';
+    } else {
+        alert("아이디와 비밀번호를 확인해주세요.");
+    }
+}
+
+function loginXhr(){
+    const xhr = new XMLHttpRequest();
+    const url = 'data/user.json';
+    const method = 'GET';
+
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === xhr.DONE && xhr.status >= 200 && xhr.status <= 400){
+            // let userInfos = JSON.parse(xhr.response.users);
+            console.log(typeof xhr.response);
+        } else {
+            alert('데이터를 전송하는데에 문제가 발생하였습니다.');
+            console.log(`HTTP Status : ${xhr.status}`);
+        }
+    }
+    xhr.open(method, url);
+    xhr.send();
+} //로컬 실행 시 CORS 문제 발생
