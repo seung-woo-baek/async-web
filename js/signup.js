6fc721e0b06eae8f04722019e9fb9504014e3b64
bsw8220 <a49208220@gmail.com>
2021-12-29 18:04:51 +0900

    onload 함수 IIFE로 수정

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,11 @@
+(function addEventToBtns(){
+    const checkDuplicationBtn = document.querySelector(".duplication-btn");
+    const signupBtn = document.getElementById("submit_btn");
+    
+    checkDuplicationBtn.addEventListener('click', checkDuplication);
+    signupBtn.addEventListener("click", signup);
+})();
+
 function checkName(form) {
     if (form.name.value == "") {
         document.getElementById('warnning_name').innerText = "필수 입력사항입니다.";
@@ -6,36 +14,11 @@ function checkName(form) {
 
     return true;
 }
+
 function checkId(form) {
     if (form.id.value == "") {
         document.getElementById('warnning_id').innerText = "필수 입력사항입니다.";
         return false;
-    }
-
-    return true;
-}
-
-const checkDuplicationBtn = document.querySelector(".duplication-btn");
-let cntDuplication = 0;
-checkDuplicationBtn.addEventListener('click', checkDuplication);
-function checkDuplication(){
-    const form = document.signup_form;
-    const existUsers = Object.keys(localStorage);
-
-    for(let i = 0; i < existUsers.length; i++){
-        const key = existUsers[i];
-        const userInfo = JSON.parse(localStorage[key]);
-        if(userInfo.userId == form.id.value){
-            document.getElementById('warnning_id').innerText = "중복된 아이디가 있습니다.";
-            document.getElementById('warnning_id').style.color = "red";
-            return false;
-        } else if(form.id.value == "") {
-            document.getElementById('warnning_id').innerText = "아이디를 입력해주세요.";
-        }else {
-            document.getElementById('warnning_id').innerText = "사용 가능한 아이디입니다.";
-            document.getElementById('warnning_id').style.color = "#1a73e8";
-            cntDuplication++;
-        }
     }
 
     return true;
@@ -50,7 +33,7 @@ function checkPassword(form) {
     const password = form.pwd.value;
 
     if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/g.test(password)) {
-        document.getElementById('warnning_pwd').innerText = "영문, 숫자, 특수문자를 최소 1자 이상, 최소 8자 이상 사용해주세요.";
+        document.getElementById('warnning_pwd').innerText = "영문, 숫자, 특수문자(@, $, !, %, *, #, ?, &)를 최소 1자 이상, 최소 8자 이상 사용해주세요.";
         return false;
     } else if (password.search(/\s/) > -1) {
         document.getElementById('warnning_pwd').innerText = "공백이 포함될 수 없습니다.";
@@ -89,8 +72,33 @@ function checkEmail(form) {
     return true;
 }
 
-let cntCheckedValue = 0;
+function checkDuplication(){
+    const form = document.signup_form;
+    const existUsers = Object.keys(localStorage);
+    let cntDuplication = 0;
+
+    for(let i = 0; i < existUsers.length; i++){
+        const key = existUsers[i];
+        const userInfo = JSON.parse(localStorage[key]);
+        if(userInfo.userId == form.id.value){
+            document.getElementById('warnning_id').innerText = "중복된 아이디가 있습니다.";
+            document.getElementById('warnning_id').style.color = "red";
+            return cntDuplication = 0;
+        } else if(form.id.value == "") {
+            document.getElementById('warnning_id').innerText = "아이디를 입력해주세요.";
+        }else {
+            document.getElementById('warnning_id').innerText = "사용 가능한 아이디입니다.";
+            document.getElementById('warnning_id').style.color = "#1a73e8";
+            cntDuplication++;
+        }
+    }
+
+    return cntDuplication;
+}
+
 function checkAllValue(form){
+    let cntCheckedValue = 0;
+
     if (checkName(form)){
         document.getElementById('warnning_name').innerText = "";
         cntCheckedValue++;
@@ -111,6 +119,8 @@ function checkAllValue(form){
         document.getElementById('warnning_email').innerText = "";
         cntCheckedValue++;
     }
+
+    return cntCheckedValue;
 }
 
 function makeUserData(form){
@@ -125,20 +135,18 @@ function makeUserData(form){
     return param;
 }
 
-const signupBtn = document.getElementById("submit_btn");
-signupBtn.addEventListener("click", signup);
 function signup() {
     const form = document.signup_form;
-    checkAllValue(form);
     const obj = makeUserData(form);
+    const cntDuplication = checkDuplication();
+    const cntCheckedValue = checkAllValue(form);
     if (cntDuplication === 0 && form.id.value !== "") {
         document.getElementById('warnning_id').innerText = "중복 검사를 해주세요.";
+        document.getElementById('warnning_id').style.color = "red"
     }
     if (cntCheckedValue === 5 && cntDuplication > 0) {
-        document.querySelector(".signup-box").style.display = "none";
+        document.querySelector(".main-container").style.display = "none";
         document.querySelector(".signup-complete-box").style.display = "flex";
         localStorage.setItem(obj.idx, JSON.stringify(obj));
-    } else {
-        return;
     }
 }
